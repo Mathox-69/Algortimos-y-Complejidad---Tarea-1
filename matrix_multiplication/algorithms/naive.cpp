f6307672a8711fea8df5df73110377940c0cca77
Mathox-_-69 <matias.vallejos.mella@gmail.com>
2026-09-11 23:20:12 -0300

    Fix naive matrix multiplication implementation

--- matrix_multiplication/algorithms/naive.cpp
+++ matrix_multiplication/algorithms/naive.cpp
@@ -2,13 +2,11 @@
 
 #include <vector>
 using namespace std;
-
 void naive_matrix_multiplication(const vector<vector<int>>&A, const vector<vector<int>>&B, vector<vector<int>>&C, int n) {
-    for(int i = 0; i < n; i++) {
-        for(int k = 0; k < n; k++) {
-            int factor = A[i][k];
-            for(int j = 0; j < n; j++) {
-                C[i][j] += factor * B[k][j];
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
